d4a5d9f06f40aa0f831bf093307e0d51a302b7ba
xiangzaizi <skyxiang.jiang@hotmail.com>
2018-06-03 13:55:03 +0800

    导入实现项目配置文件, 与默认配置文件相交互

---
+++ code/settings.py
@@ -0,0 +1,5 @@
+# -*- coding:utf-8 -*-
+
+
+# 修改默认配置文件名, 使用时间项目名记录日志信息
+DEFAULT_LOG_FILENAME = 'baidu.log'
--- scrapy_option/conf/default_settings.py
+++ scrapy_option/conf/default_settings.py
@@ -11,3 +11,12 @@ DEFUALT_LOG_DATEFMT = '%Y-%m-%d %H:%M:%S'  # 默认时间格式
 DEFAULT_LOG_FILENAME = 'log.log'    # 默认日志文件名称
 
 
+# 使用项目中的配置文件, 修改默认配置文件内容
+from settings import *
+"""
+1. 此处位置不用管settings爆红, 在项目导入框架时sys.path会沿着项目settings找有就会以项目配置文件配置内容运行
+2. 没有就会按照框架内部配置的文件调试
+3. from code.settings import  *--->因为code项目文件夹仅是测试文件夹可以随时换,框架内不宜放变化的参数
+4. import sys  print(sys.path)--->查看当前项目运行时导包的顺序验证3
+"""
+
