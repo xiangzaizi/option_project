d5b554e007daa43fb1e7d81d6dd8edcca38ce28f
xiangzaizi <skyxiang.jiang@hotmail.com>
2018-06-03 16:29:41 +0800

    对响应进行升级支持多种解析方法:xpath josn re_findall

--- scrapy_option/http/response.py
+++ scrapy_option/http/response.py
@@ -1,6 +1,8 @@
 # -*- coding:utf-8 -*-
 """封装Response对象"""
-
+import json
+import re
+from lxml import etree
 
 class Response(object):
     """框架内置Response对象"""
@@ -9,3 +11,18 @@ class Response(object):
         self.status_code = status_code
         self.headers = headers
         self.body = body
+
+    def xpath(self, rule):
+        html_obj = etree.HTML(self.body)
+        return html_obj.xpath(rule)
+
+    # 将网页返回的json字符串, 转为python数据类型
+    @property
+    def json(self):
+        return json.loads(self.body)
+
+    def re_findall(self, rule, string=None):
+        if string is None:
+            string = self.body
+        return re.findall(rule, string)
+
