f0787241155a3b9f25f83d0d4d8598b0bf31c28e
xiangzaizi <skyxiang.jiang@hotmail.com>
2018-06-03 13:25:38 +0800

    1. 添加下载中间件process_request() process_response() 2. 添加爬虫中间件process_request() process_item() 中间件添加完毕

--- scrapy_option/core/engine.py
+++ scrapy_option/core/engine.py
@@ -14,6 +14,10 @@ from .downloader import Downloader
 from .pipeline import Pipeline
 from scrapy_option.item import Item
 
+# 3. 导入中间件middlwares
+from scrapy_option.middlewares.spider_middlewares import SpiderMiddlewares
+from scrapy_option.middlewares.downloader_middlewares import DownloaderMiddlewares
+
 
 class Engine(object):
     def __init__(self):
@@ -22,6 +26,10 @@ class Engine(object):
         self.scheduler = Scheduler()
         self.downloader = Downloader()
         self.pipeline = Pipeline()
+
+        # 初始化中间件文件
+        self.spider_middlewares = SpiderMiddlewares()
+        self.downloader_middlewares = DownloaderMiddlewares()
 
     def start(self):
         # 启动engine
@@ -42,19 +50,35 @@ class Engine(object):
             if request is None:
                 break
 
+            ### 3.1 将请求经过下载中间件预处理--->请求
+            request = self.downloader_middlewares.process_request(request)
+
             # 4. 将请求交个下载器
             response = self.downloader.get_response(request)
+
+            ### 4.1 将响应经过下载中间件预处理--->响应
+            response = self.downloader_middlewares.process_response(response)
 
             # 5. 得到响应对象交给spider解析数据
             result = self.spider.parse_request(response)
 
             # 6. 判断解析出来的结果进行在判断
             if isinstance(result, Request):
+
+                ### 6.1 如果是请求对象, 交给爬虫中间件预处理, 添加请求入队列
+                result = self.spider_middlewares.process_request(result)
+
                 # 是请求继续入队列请求数据
                 self.scheduler.add_request(result)
+
             elif isinstance(result, Item):
+
+                ### 6.2 如果是Item数据, 爬虫中间件预处理,在交给管道
+                result = self.spider_middlewares.process_item(result)
+
                 # 得到请求的数据转到管道, 进行存储
                 self.pipeline.process_item(result)
+
             else:
                 raise Exception("Error: parse返回的数据不能被处理")
 
