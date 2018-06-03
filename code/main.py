f5467d0caa2c22314b5eff19c8d7651810bd8134
xiangzaizi <skyxiang.jiang@hotmail.com>
2018-06-03 17:10:05 +0800

    抓取豆瓣250电影数据测试

--- code/main.py
+++ code/main.py
@@ -1,9 +1,12 @@
 # -*- coding:utf-8 -*-
 from scrapy_option.core.engine import Engine
-from spider import BaiduSpider
+from spider import BaiduSpider, DoubanSpider
 if __name__ == '__main__':
     # 导入项目爬虫, 并创建对象
-    spider = BaiduSpider()
+    # spider = BaiduSpider()
+
+    # 豆瓣电影测试--1
+    spider = DoubanSpider()
 
     # 将爬虫对象传入到engine中
     engine = Engine(spider)
--- code/spider.py
+++ code/spider.py
@@ -1,6 +1,7 @@
 # -*- coding:utf-8 -*-
 """自定义spider测试"""
 from scrapy_option.core.spider import Spider
+from scrapy_option.http.request import Request
 
 
 class BaiduSpider(Spider):
@@ -9,3 +10,20 @@ class BaiduSpider(Spider):
         "http://www.baidu.com/",
         "http://news.baidu.com/"
     ]
+
+
+class DoubanSpider(Spider):
+    # 豆瓣电影抓取
+    headers = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko"}
+
+    def start_request(self):
+        start_urls = ["https://movie.douban.com/top250?start=" + str(page) for page in range(0, 226, 25)]
+
+        for start_url in self.start_urls:
+            yield Request(start_url, headers=self.headers)  # 这里就会直接交给engine内部对多url请求做遍历
+
+    def parse(self, response):
+        for node in response.xpath("//div[@class='hd']"):
+            title = node.xpath(".//span[@class='title'][1]/text()")[0]
+            link = node.xpath("./a/@href")[0]
+            yield Request(link, headers=self.headers)
