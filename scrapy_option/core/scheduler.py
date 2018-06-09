c035070c3d6c3556ea541f0371c05c589a7fb2a7
xiangzaizi <skyxiang.jiang@hotmail.com>
2018-06-09 14:26:07 +0800

    1. 添加指纹 2. 通过指纹来去重

--- scrapy_option/core/scheduler.py
+++ scrapy_option/core/scheduler.py
@@ -1,6 +1,7 @@
 # -*- coding:utf-8 -*-
 """创建可兼容py2_py3的队列"""
 import six
+from scrapy_option.utils.log import logger
 # 方式一
 try:
     from Queue import Queue # py2
@@ -19,11 +20,16 @@ class Scheduler(object):
         self._filter_set = set()  # 保存指纹, 目前是测试的url, 使用set()对请求的url进行去重
 
     def add_request(self, request):
-        # 对请求去重, 并添加不重复的请求到队列中
-        if not self._filter_request(request):
+        # ***2.生成指纹(唯一性)
+        fp = self._gen_fingerprint(request)
+
+        # ***1. 对请求去重, 并添加不重复的请求到队列中
+        if not self._filter_request(fp, request):
+            logger.info(u"添加请求(not filter)成功:[{}]<{}>".format(request.method, request.url))
             self.queue.put(request)
             # add请求到队列中
-            self._filter_set.add(request.url)
+            # self._filter_set.add(request.url)
+            self._filter_set.add(fp)  # 通过指纹来去重
 
     def get_request(self):
         # 获取排队中的请求--->进入下载器下载
@@ -32,9 +38,11 @@ class Scheduler(object):
         except:
             return None
 
-    def _filter_request(self, request):
+    def _filter_request(self, fp, request):
         # 判断是否是重复的请求, 如果是重复的返回True, 否则返回False
-        if request.url in self._filter_set:
+        # if request.url in self._filter_set:
+        if fp in self._filter_set:
+            logger.info(u"重复请求: [{}],<{}>".format(request.method, request.url))
             return True
         else:
             return False
